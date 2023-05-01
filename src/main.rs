897be262cd53a3bd171bfd1ef52ed867d59aca7c
DESKTOP-B5IRF34\shuoya <timehet556947@gmail.com>
2023-05-01 15:46:22 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "{},{},{},{},{}",
+        arr_square()[0],
+        arr_square()[1],
+        arr_square()[2],
+        arr_square()[3],
+        arr_square()[4]
+    );
+}
+fn arr_square() -> [i32; 5] {
+    let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
+    for i in 0..arr.len() {
+        arr[i] *= arr[i];
+    }
+    return arr;
 }
